588907b2bd33786d58df046250df617cb5fbc505
Roshan Titus <roshan.t.titus@oracle.com>
2017-08-09 23:29:29 +0530

    First commit

---
+++ src/main/java/com/rsinc/estore/EstoreAdminApplication.java
@@ -0,0 +1,21 @@
+package com.rsinc.estore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class EstoreAdminApplication  extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoreAdminApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EstoreAdminApplication.class);
+	}	
+}
